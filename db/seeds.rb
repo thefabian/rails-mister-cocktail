1e4225b19792fd0cd971420aa7b3ef2451e5e061
Fabian Bitta <fabian.bitta@gmail.com>
2018-08-09 17:17:09 +0200

    Update ruby on rails with models, controllers, views and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+data = open(url).read
+cocktail = JSON.parse(data)
+
+
+cocktail.values.first.each do |ingredient|
+Ingredient.create(name: ingredient.values.pop)
+end
